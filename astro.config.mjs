7a05e413ed52df7c7779ab34c8e8acfb91361f47
Marisol Henao <henao.1213@gmail.com>
2025-08-20 14:25:58 -0500

    Chore configuración de alias, instalación de dependencias generales y específicas como tailwind css y mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(),],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  },
+  integrations: [mdx()],
+});
